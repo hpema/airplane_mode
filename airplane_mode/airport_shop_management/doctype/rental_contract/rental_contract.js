ce4cdff9eaec0bd805df0f34a43f2ee4188220a4
Hemant Pema <hemant@pema.co.za>
2024-04-22 23:55:39 +0400

    Contract submit working

--- airplane_mode/airport_shop_management/doctype/rental_contract/rental_contract.js
+++ airplane_mode/airport_shop_management/doctype/rental_contract/rental_contract.js
@@ -5,36 +5,92 @@ frappe.ui.form.on("Rental Contract", {
 	refresh(frm) {
         if(frm.is_new()){
              frappe.db.get_single_value("Airport Shop Settings", 'default_rent_amount').then((rent_amount) => {
-               if (frm.rental_amount == 0 || frm.rental_amount == null){
+               if (frm.doc.rental_amount == 0 || frm.doc.rental_amount == null){
                    frm.set_value("rental_amount", rent_amount);
-                   console.log(frm.rental_amount);
+                   console.log(frm.doc.rental_amount);
                }
             });
         }   
-        
+        if(frm.doc.end_date==null){
+            console.log(addMonthsToDate(frm.doc.start_date, 12));
+            frm.set_value("end_date",addMonthsToDate(frm.doc.start_date, 12));
+        }
+        frm.refresh_field("add_ons");
 	},
+    end_date(frm){
+        if(frm.doc.end_date <= frm.doc.start_date){
+            console.log("End date to be in the future of start date.")
+        }else{
+            let enddate = new Date(frm.doc.end_date);
+            let startdate = new Date(frm.doc.startdate);
+
+        }
+    },
+    start_date(frm){
+            frm.set_value("end_date",addMonthsToDate(frm.doc.start_date, 12));
+    },
+    on_submit(frm){
+        frm.reload_doc();
+    }
+    
 });
 
 
 
-frappe.ui.form.on("Rent Due", {
+frappe.ui.form.on("Rent", {
     payment_received(frm,cdt, cdn){
         let row = frappe.get_doc(cdt,cdn); 
         let idx = row.idx;
-        //let item_added = row.item 
-        
-        //if(item_added !=''){
-        //    for(let item of frm.doc.add_ons){
-        //        if(item.item == item_added && item.idx!=idx){
-        //            //console.log("Duplicate: ", row.name);
-        //            frm.fields_dict.add_ons.grid.grid_rows_by_docname[cdn].remove();
-        //            frappe.throw("Item: " + item_added + " is not unique. Add on already on the list");
-        //        }
-        //    }
-       // }
-       console.log("Burron pressed on row " + idx);
+        frappe.call({
+            method: 'airplane_mode.airport_shop_management.doctype.rental_contract.rental_contract.receive_payment',
+            args: {
+                doctype: cdt,
+                name: cdn
+            },
+            // disable the button until the request is completed
+            btn: $('.primary-action'),
+            // freeze the screen until the request is completed
+            freeze: true,
+            callback: (r) => {
+                if (r.message){
+                    //frm.trigger("refresh");
+                    frm.reload_doc();
+                    //console.log(r.message);
+                    //console.log(frm.fields_dict.rent_due.grid.grid_rows_by_docname[cdn]);
+                    //frm.fields_dict.rent_due.grid.grid_rows_by_docname[cdn].refresh()
+                    //frm.refresh();
+                }else
+                {
+                    frappe.msgprint("Rent already paid")
+                }
+            },
+            error: (r) => {
+                // on error
+                console.log("error");
+            }
+        })
     },
    // amount(frm,cdt, cdn){
    //     frm.trigger("update_total_amount");
    // }
  })
+
+function addMonthsToDate(startDate, monthsToAdd) {
+    // Create a new Date object from the start date
+    var date = new Date(startDate);
+  
+    // Add the number of months to the date's month property
+    date.setMonth(date.getMonth() + monthsToAdd);
+    
+    // Return the formatted end date string
+    return formatDate(date);
+  }
+  
+ function formatDate(date) {
+    var year = date.getFullYear();
+    var month = String(date.getMonth() + 1).padStart(2, '0');
+    var day = String(date.getDate()).padStart(2, '0');
+  
+    // Return formatted date string (e.g., YYYY-MM-DD)
+    return year + "-" + month + "-" + day;
+  }
--- airplane_mode/airport_shop_management/doctype/rental_contract/rental_contract.py
+++ airplane_mode/airport_shop_management/doctype/rental_contract/rental_contract.py
@@ -4,6 +4,10 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils import *
+from frappe.query_builder import DocType
+from frappe.query_builder.functions import Count
+from pypika.terms import Case
+from datetime import date, timedelta
 
 class RentalContract(Document):
 	def validate(self):
@@ -15,3 +19,53 @@ class RentalContract(Document):
 
 		# Trigger print format using frappe.msgprint
 		#frappe.get_print("Rent Reciept", self.name)
+	def before_submit(self):
+		date = frappe.utils.add_days(frappe.utils.add_months(self.start_date,1),-1)
+		self.receipt_date = frappe.utils.add_months(date,1)
+		self.total_amount_due = self.rental_amount*12
+		self.total_amount_paid = 0 
+		self.outstanding_amount = self.total_amount_due
+  
+	def on_submit(self):
+		#print(self.name)
+		Rent = frappe.qb.DocType("Rent")
+		q = (
+			frappe.qb.from_(Rent)
+			.select(Rent.name)
+			.where(Rent.parent == self.name)
+		).run(as_dict=True)
+		#print(q)
+		for r in q:
+			#print(r)
+			frappe.delete_doc("Rent",r.name)
+		
+		date = frappe.utils.add_days(frappe.utils.add_months(self.start_date,1),-1)
+		for i in range(12):
+			#print(frappe.utils.add_months(date,i))
+			rent = frappe.get_doc({
+				"doctype": "Rent",
+				"rental_amount": self.rental_amount,
+				"parent": self.name,
+				"parenttype": "Rental Contract",
+				"parentfield": "rent_due",
+				"due_date": frappe.utils.add_months(date,i),
+				"idx":i+1
+       			})
+			rent.insert()
+		#self.reload()
+
+@frappe.whitelist()
+def receive_payment(doctype, name):
+    line = frappe.get_doc(doctype,name)#qb.update(doctype).,name)
+    if line.paid_amount == 0:
+        #line.paid_amount = line.rental_amount
+        line.db_set("paid_amount",line.rental_amount)
+        line.db_set("paid_on",frappe.utils.today())
+        parent = frappe.get_doc(line.parenttype,line.parent)
+        parent.db_set("receipt_date", line.paid_on)
+        
+        line.save()
+        frappe.db.commit()
+        return line.paid_amount
+    else:
+        return 0
